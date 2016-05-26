9b8c6142541b9eb5f3c09aa7324ac5dfdf83c192
Martin Emde <me@martinemde.com>
2016-05-26 11:05:09 -0700

    Refactor Geom to the tuple version
    I don't like this version as much, so I did not
    make the other methods private

--- lib/geom.ex
+++ lib/geom.ex
@@ -8,18 +8,8 @@ defmodule Geom do
   and assuming a rectangle, just return 1.
 
   Example:
-    iex> Geom.area()
-    1
   """
   def area(), do: 1
-
-  @doc """
-  Find the area of a shape with default values
-    iex> Geom.area(7)
-    7
-  """
-  def area(x)     when is_integer(x),  do: area(:rectangle, x, 1)
-  def area(shape) when is_atom(shape), do: area(shape, 1, 1)
 
   @doc """
   Find the area of a shape.
@@ -31,27 +21,27 @@ defmodule Geom do
     84
     iex> Geom.area(7, 5)
     35
+    iex> Geom.area(7)
+    7
+    iex> Geom.area()
+    1
+    iex> Geom.area({:rectangle, 3, 4})
+    12
+    iex> Geom.area({:triangle, 3, 5})
+    7.5
+    iex> Geom.area({:ellipse, 2, 4})
+    25.132741228718345
   """
+  def area({shape, x, y}), do: area(shape, x, y)
+  def area(x)     when is_integer(x),  do: area(:rectangle, x, 1)
+  def area(shape) when is_atom(shape), do: area(shape, 1, 1)
+
   def area(shape, x) when is_atom(shape), do: area(shape, x, 1)
   def area(x, y)     when is_integer(x),  do: area(:rectangle, x, y)
 
-  @doc """
-    iex> Geom.area(:rectangle, 3, 4)
-    12
-  """
-  def area(:rectangle, x, y), do: x * y
-
-  @doc """
-  Example:
-    iex> Geom.area(:triangle, 3, 5)
-    7.5
-  """
-  def area(:triangle, x, y), do: x * y / 2.0
-
-  @doc """
-  Example:
-    iex> Geom.area(:ellipse, 2, 4)
-    25.132741228718345
-  """
-  def area(:ellipse, x, y), do: :math.pi * x * y
+  @spec area(atom(), number(), number()) :: number()
+  def area(:rectangle, x, y) when x >= 0 and y >= 0, do: x * y
+  def area(:triangle, x, y)  when x >= 0 and y >= 0, do: x * y / 2.0
+  def area(:ellipse, x, y)   when x >= 0 and y >= 0, do: :math.pi * x * y
+  def area(_, _, _), do: 0
 end
